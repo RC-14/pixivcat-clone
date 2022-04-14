3764597a4cb1bb174ddae8ec280a2f12ac2150cf
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-04-14 12:26:21 +0200

    use config file for config

--- index.js
+++ index.js
@@ -8,7 +8,30 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 
-const port = process.env.PORT || 8080;
+// Read config from pixivcat-clone.config as JSON
+// ONLY CONFIGURE FROM CONFIG FILE!
+// DO NOT CONFIGURE DIRECTLY IN CODE!
+const config = JSON.parse(fs.readFileSync('pixivcat-clone.config', 'utf8')) || {
+    Port: 8080,
+    UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
+    Cookies: "", // Default to empty string (no cookies) because I don't want to share my account
+};
+
+// Validate config
+if (typeof config !== 'object') {
+    throw new Error('Invalid config');
+} else if (typeof config.Port !== 'number' || config.Port < 1 || config.Port > 65535) {
+    throw new Error('Invalid config.Port');
+} else if (typeof config.UserAgent !== 'string' || !config.UserAgent) {
+    throw new Error('Invalid config.UserAgent');
+} else if (typeof config.Cookies !== 'string' || !config.Cookies) {
+    console.warn('No or invalid config.Cookies');
+}
+
+const port = config.Port;
+const userAgent = config.UserAgent;
+const cookies = typeof config.Cookies === 'string' && config.Cookies ? config.Cookies : null;
+
 const pathExpression = /^\/(\d+)(?:-(\d+))?\.jpg$/;
 
 const parsePath = (path) => {
